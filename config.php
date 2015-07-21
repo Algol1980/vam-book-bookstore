dfcea9e729e0b0e83ce9f3c019aa5bd05fb87552
algol1980 <algol1980@yandex.ru>
2015-07-21 19:10:11 +0300

    Певая загрузка

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/vam-book/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/vam-book/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/localhost/vam-book/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/localhost/vam-book/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/localhost/vam-book/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/localhost/vam-book/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/localhost/vam-book/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/localhost/vam-book/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/localhost/vam-book/system/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/localhost/vam-book/system/download/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/localhost/vam-book/system/upload/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/localhost/vam-book/system/modification/');
+define('DIR_LOGS', 'C:/OpenServer/domains/localhost/vam-book/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'algol1980');
+define('DB_PASSWORD', '198022');
+define('DB_DATABASE', 'train_db20');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ vam-book-back/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/vam-book/vam-book-back/');
+define('HTTP_CATALOG', 'http://localhost/vam-book/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/vam-book/vam-book-back/');
+define('HTTPS_CATALOG', 'http://localhost/vam-book/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/localhost/vam-book/vam-book-back/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/localhost/vam-book/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/localhost/vam-book/vam-book-back/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/localhost/vam-book/vam-book-back/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/localhost/vam-book/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/localhost/vam-book/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/localhost/vam-book/system/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/localhost/vam-book/system/download/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/localhost/vam-book/system/upload/');
+define('DIR_LOGS', 'C:/OpenServer/domains/localhost/vam-book/system/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/localhost/vam-book/system/modification/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/localhost/vam-book/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'algol1980');
+define('DB_PASSWORD', '198022');
+define('DB_DATABASE', 'train_db20');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
